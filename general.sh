53ef6110fd94e7d898ab02ff92cc4c2bde93c812
XeZZ <distruct3r@web.de>
2018-04-02 17:34:58 +0200

    general purpose script

---
+++ general.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y nano htop git
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libevent-dev
+sudo apt-get install -y libminiupnpc-dev libgmp-dev
+sudo apt-get install -y autoconf
+sudo apt-get install -y automake unzip
+sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+
+cd /var
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo free
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+cd
+
+sudo apt-get install -y ufw fail2ban
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw logging on
+echo "y" | sudo ufw enable
+sudo ufw status
+
+systemctl enable fail2ban >/dev/null 2>&1
+systemctl start fail2ban >/dev/null 2>&1
+
+mkdir -p ~/bin
+echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
+source ~/.bashrc
