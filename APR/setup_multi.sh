5e7bfcde4c3aafa8046dbf88d131d5fe74451f1f
XeZZ <distruct3r@web.de>
2018-06-26 21:12:34 +0200

    revert

--- APR/setup_multi.sh
+++ APR/setup_multi.sh
@@ -43,6 +43,14 @@ if [[ $DOSETUP =~ "y" ]] ; then
   sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
   cd
 
+  ## COMPILE AND INSTALL
+  wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-cli
+  wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-qt
+  wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-tx
+  wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoind
+  sudo chmod 755 aprcoin*
+  sudo mv aprcoin* /usr/bin
+
   sudo apt-get install -y ufw
   sudo ufw allow ssh/tcp
   sudo ufw limit ssh/tcp
@@ -55,43 +63,70 @@ if [[ $DOSETUP =~ "y" ]] ; then
   source ~/.bashrc
 fi
 
-## COMPILE AND INSTALL
-wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-cli
-wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-qt
-wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-tx
-wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoind
-sudo chmod 755 aprcoin*
-sudo mv aprcoin* /usr/bin
+## Setup conf
+mkdir -p ~/bin
+IP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+wget https://github.com/XeZZoR/scripts/raw/master/APR/peers.dat -O apr_peers.dat
+NAME="aprcoin"
 
-CONF_DIR=~/.aprcoin/
-mkdir $CONF_DIR
-CONF_FILE=aprcoin.conf
-PORT=3133
 
-wget https://github.com/XeZZoR/scripts/raw/master/APR/peers.dat -O $CONF_DIR/peers.dat
+MNCOUNT=""
+re='^[0-9]+$'
+while ! [[ $MNCOUNT =~ $re ]] ; do
+   echo ""
+   echo "How many nodes do you want to create on this server?, followed by [ENTER]:"
+   read MNCOUNT
+done
 
-IP=$(curl -s4 icanhazip.com)
+for i in `seq 1 1 $MNCOUNT`; do
+  echo ""
+  echo "Enter alias for new node"
+  read ALIAS  
 
-echo ""
-echo "Enter masternode private key for node $ALIAS"
-read PRIVKEY
+  echo ""
+  echo "Enter port for node $ALIAS (Any valid free port matching config from steps before: i.E. 8001)"
+  read PORT
 
-echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
-echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
-echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
-echo "listen=1" >> $CONF_DIR/$CONF_FILE
-echo "server=1" >> $CONF_DIR/$CONF_FILE
-echo "daemon=1" >> $CONF_DIR/$CONF_FILE
-echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
-echo "maxconnections=256" >> $CONF_DIR/$CONF_FILE
-echo "masternode=1" >> $CONF_DIR/$CONF_FILE
-echo "" >> $CONF_DIR/$CONF_FILE
-echo "" >> $CONF_DIR/$CONF_FILE
-echo "port=$PORT" >> $CONF_DIR/$CONF_FILE
-echo "masternodeaddr=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
-echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
+  echo ""
+  echo "Enter RPC Port (Any valid free port: i.E. 9001)"
+  read RPCPORT
 
-echo "addnode=[2001:0:4137:9e76:144f:1354:bb79:a4a3]:3133" >> $CONF_DIR/$CONF_FILE
+  echo ""
+  echo "Enter masternode private key for node $ALIAS"
+  read PRIVKEY
+
+  ALIAS=${ALIAS,,}
+  CONF_DIR=~/.${NAME}_$ALIAS
+  CONF_FILE=aprcoin.conf
+
+  # Create scripts
+  echo '#!/bin/bash' > ~/bin/${NAME}d_$ALIAS.sh
+  echo "${NAME}d -daemon -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' >> ~/bin/${NAME}d_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/${NAME}-cli_$ALIAS.sh
+  echo "${NAME}-cli -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' >> ~/bin/${NAME}-cli_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/${NAME}-tx_$ALIAS.sh
+  echo "${NAME}-tx -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' >> ~/bin/${NAME}-tx_$ALIAS.sh 
+  chmod 755 ~/bin/${NAME}*.sh
+
+  mkdir -p $CONF_DIR
+  echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> ${NAME}.conf_TEMP
+  echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> ${NAME}.conf_TEMP
+  echo "rpcallowip=127.0.0.1" >> ${NAME}.conf_TEMP
+  echo "rpcport=$RPCPORT" >> ${NAME}.conf_TEMP
+  echo "listen=1" >> ${NAME}.conf_TEMP
+  echo "server=1" >> ${NAME}.conf_TEMP
+  echo "daemon=1" >> ${NAME}.conf_TEMP
+  echo "logtimestamps=1" >> ${NAME}.conf_TEMP
+  echo "maxconnections=256" >> ${NAME}.conf_TEMP
+  echo "masternode=1" >> ${NAME}.conf_TEMP
+  echo "" >> ${NAME}.conf_TEMP
+
+  echo "" >> ${NAME}.conf_TEMP
+  echo "port=$PORT" >> ${NAME}.conf_TEMP
+  echo "masternodeaddr=$IP:$PORT" >> ${NAME}.conf_TEMP
+  echo "masternodeprivkey=$PRIVKEY" >> ${NAME}.conf_TEMP
+
+  echo "addnode=[2001:0:4137:9e76:144f:1354:bb79:a4a3]:3133" >> $CONF_DIR/$CONF_FILE
 echo "addnode=[2001:0:9d38:6abd:286e:1da0:230f:9a1]:3133" >> $CONF_DIR/$CONF_FILE
 echo "addnode=209.250.240.35:3133" >> $CONF_DIR/$CONF_FILE
 echo "addnode=64.154.38.181:3133" >> $CONF_DIR/$CONF_FILE
@@ -122,6 +157,11 @@ echo "addnode=[2001:0:5ef5:79fb:30c7:e6f:e73b:d6bc]:64296" >> $CONF_DIR/$CONF_FI
 echo "addnode=[2001:0:9d38:953c:2877:26a0:ed13:4371]:50111" >> $CONF_DIR/$CONF_FILE
 echo "addnode=[2001:0:4137:9e76:8e1:15b4:a310:7243]:3133" >> $CONF_DIR/$CONF_FILE
 
-sudo ufw allow $PORT/tcp
 
-aprcoind -daemon
+  sudo ufw allow $PORT/tcp
+
+  mv ${NAME}.conf_TEMP $CONF_DIR/${NAME}.conf
+  cp apr_peers.dat $CONF_DIR/peers.dat
+  
+  sh ~/bin/${NAME}d_$ALIAS.sh
+done
