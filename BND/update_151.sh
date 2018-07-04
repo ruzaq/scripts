8dc9a29a143bb35334d7db2eed80f7d15923bc34
XeZZ <distruct3r@web.de>
2018-07-04 18:47:47 +0200

    new version BND

--- BND/update_151.sh
+++ BND/update_151.sh
@@ -7,8 +7,10 @@ rm bin/blocknode* /usr/bin/blocknode*
 mv blocknode-1.5.1/bin/blocknode* /usr/bin
 blocknode-cli stop
 sleep 10
+cd .blocknode
+rm -r backups/ banlist.dat blocks/ budget.dat chainstate/ db.log debug.log fee_estimates.dat masternode.conf mncache.dat mnpayments.dat sporkat zerocoin/
 blocknoded
 echo "Wait for blockchain to restart...."
-sleep 30
+sleep 60
 blocknode-cli masternode status
 echo "Please restart on gui if status is not 4!"
